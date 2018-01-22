d1abd3f82564f7763836230979b5d78050b31176
Tiffany Lam <tlam5@sbcglobal.net>
2018-01-22 10:38:00 -0800

    Include error handling on quotefetch

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener( 'DOMContentLoaded', () => {
     // ==== Once HTML is there =====
     utils.getData( 'get', 'http://ron-swanson-quotes.herokuapp.com/v2/quotes' )
-         .then( res => document.getElementById('quote').innerHTML = `"${res}"` )
-         // do something if res doesn't come back the way you expect
+         .then( quote => document.getElementById('quote').innerHTML = `"${quote}"` )
+         .catch(err => console.log(err));
 } )
